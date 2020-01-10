d6375924343c2c797d884903344c6b506df702e6
Logan Hentschel <hentscht@student.psdr3.org>
2020-01-10 12:23:48 -0600

    button added, working on new window & printing variables

--- unit_2_1/a214_simple_window1.py
+++ unit_2_1/a214_simple_window1.py
@@ -4,6 +4,10 @@ root = tk.Tk()
 root.wm_geometry("400x200")
 root.title('Authorization')
 # # #
+def login_button_press():
+    print('pressed')
+
+# # #
 lbl_username = tk.Label(root, text='Username:')
 lbl_username.pack()
 #
@@ -17,7 +21,8 @@ ent_password = tk.Entry(root, bd=3)
 ent_password.pack(pady=5)
 ###PRINT TO TERMINAL USERNAME AND PASS AFTER SUBMISSION
 
-#
+log_button = tk.Button(root, text="Login", command=login_button_press)
+log_button.pack()
 
 #
 
