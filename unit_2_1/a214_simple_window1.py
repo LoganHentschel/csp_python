96fc1becd2ec6acfabee3c9292ca3223ab3f3dbf
Logan Hentschel <hentscht@student.psdr3.org>
2020-01-13 11:52:11 -0600

    finished all

--- unit_2_1/a214_simple_window1.py
+++ unit_2_1/a214_simple_window1.py
@@ -12,17 +12,17 @@ def login_button_press():
 # # #
 lbl_username = tk.Label(root, text='Username:')
 lbl_username.pack()
-#
+
 ent_username = tk.Entry(root, bd=3)
 ent_username.pack(pady=5)
 ###
 lbl_password = tk.Label(root,text="Password:",font="Courier")
 lbl_password.pack()
-#
+
 ent_password = tk.Entry(root, bd=3)
 ent_password.pack(pady=5)
-###PRINT TO TERMINAL USERNAME AND PASS AFTER SUBMISSION
 
+###
 log_button = tk.Button(root, text="Login", command=login_button_press)
 log_button.pack()
 
