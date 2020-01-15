7b773c3a59104cc1d9924d84366d1ca99dfa80eb
Logan Hentschel <hentscht@student.psdr3.org>
2020-01-15 11:46:58 -0600

    all done except for big bug w capital/lowercase

--- unit_2_1/a214_simple_window1.py
+++ unit_2_1/a214_simple_window1.py
@@ -1,7 +1,9 @@
 #   a113_TR_simple_window1.py
 import tkinter as tk
+import string
+#
 root = tk.Tk()
-root.wm_geometry("400x200")
+root.wm_geometry("400x250")
 root.title('Authorization')
 # # #
 def login():
@@ -10,26 +12,28 @@ def login():
     ent_username.delete(0, "end")
 
 def password_validation():
-    #character_amount_check()
-    upcase_lowcase_check()
+    character_amount_check()
 
 def character_amount_check():
     if len(ent_password.get()) < 8:
-        print('Please have at least 8 characters.')
+        result_label.config(text='Please have at least 8 characters.')
         ent_password.delete(0, "end")
         ent_username.delete(0, "end")
     else:
-        #num_included_check()
-        pass
+        num_included_check()
 
 def num_included_check():
-    print('num check')
-    #upcase_lowcase_check()
+    for char in ent_password.get():
+        if char.isdigit():
+            upcase_lowcase_check()
+        else:
+            result_label.config(text='Please include at least one number.')
+            ent_password.delete(0, "end")
 
 def upcase_lowcase_check():
     upper = 0
     lower = 0
-    print('upper lower check')
+    print('checked nums')
     for char in ent_password.get():
         if upper  < 1:
             if char.isupper():
@@ -37,15 +41,20 @@ def upcase_lowcase_check():
         if lower < 1:
             if char.islower():
                 lower += 1
-    print(upper, lower, 'clear')
-            
-
-    #symbol_check()
-  
+        if lower and upper == 1:
+            print('syyyyyymbols')
+            symbol_check()
+    else:
+        result_label.config(text='Please include both a Captial and Lowercase letter.')
+        ent_password.delete(0, "end")
 
 def symbol_check():
     print('symbol check')
-    #login()
+    for char in ent_password.get():
+        if char in string.punctuation:
+            login()
+        else:
+            result_label.config(text='Please include a symbol.')
 
 # # #
 lbl_username = tk.Label(root, text='Username:')
@@ -53,16 +62,22 @@ lbl_username.pack(pady=5)
 
 ent_username = tk.Entry(root, bd=3)
 ent_username.pack(pady=5)
-###
+#
 lbl_password = tk.Label(root,text="Password:",font="Courier")
 lbl_password.pack()
 
 ent_password = tk.Entry(root, bd=3)
 ent_password.pack(pady=5)
 
-###
+#
 log_button = tk.Button(root, text="Login", command=password_validation)
 log_button.pack()
+#
+result_label = tk.Label(root)
+result_label.pack(pady=5)
+#
+sign_up = tk.Button(root, text="Sign Up",)
+sign_up.pack(pady=5)
 
 # # #
 root.mainloop()
