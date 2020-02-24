909a3432078ed3ae0631dc0e04c9c7259a9e549a
Logan Hentschel <hentscht@student.psdr3.org>
2020-02-24 01:47:16 -0600

    working on light graph. like the morse, it shows, but looks kinda weird

--- Unit_3_1/light.py
+++ Unit_3_1/light.py
@@ -1,6 +1,43 @@
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
+
+path = 'data_files/light.csv'
+
+with open(path) as f:
+    data = f.read().split('\n')
+
+# # # # #
+time_list = []
+data_615nm_list = []
+data_525nm_list = []
+data_465nm_list = []
+
+# # #
+for line in data[1:]:
+	split_line = line.split(',')
+	# # #
+	for line in data:
+		time_list.append(split_line[0])
+		data_615nm_list.append(split_line[1])
+		data_525nm_list.append(split_line[2])
+		data_465nm_list.append(split_line[3])
+
+# # #
+plt.grid(b=None, which='major', axis='both')
+#plt.grid(color='grey')
+
+plt.plot(time_list, data_615nm_list, c='red')
+plt.plot(time_list, data_525nm_list, c='green')
+plt.plot(time_list, data_465nm_list, c='blue')
+
+plt.title('Light Sensing')
+plt.xlabel('Time (Seconds)')
+plt.ylabel('light...?')
+
+# # #
+plt.show()
 
 
-fig, ax = plt.subplots(1,1)
-ax.plot([[1,2,3], [4,5,6]])
-plt.showfig()
+# # # # #
+#fig, ax = pyplot.subplots(1,1)
+#ax.plot([[1,2,3], [4,5,6]])
+#pyplot.show()
