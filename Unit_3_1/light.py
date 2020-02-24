44c18b447082951d438f7f20d27340edab185713
Logan Hentschel <hentscht@student.psdr3.org>
2020-02-24 01:09:37 -0600

    finnally finished morse code graph. it shows up, but there seems to be too much data...

---
+++ Unit_3_1/light.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as pyplot
+
+
+fig, ax = plt.subplots(1,1)
+ax.plot([[1,2,3], [4,5,6]])
+plt.showfig()
--- Unit_3_1/morse_code_graph.py
+++ Unit_3_1/morse_code_graph.py
@@ -1,5 +1,51 @@
-''' CSP ASSIGNMENT
-Morse code data:
-	put into sheet & create chart'''
+# # # # # # # # # #
+import matplotlib.pyplot as plt
 
-#
+path = 'data_files/morse_graph.csv'
+
+with open(path) as f:
+    data = f.read().split('\n')
+
+# # #
+time_list = []
+data4_list = []
+data6_list = []
+data9_list = []
+data12_list = []
+data13_list = []
+data14_list = []
+# # #
+
+#print(data)
+
+for line in data[1:]:
+	split_line = line.split(',')
+    #
+	for line in data:
+		time_list.append(split_line[0])
+		data4_list.append(split_line[1])
+		data6_list.append(split_line[2])
+		data9_list.append(split_line[3])
+		data12_list.append(split_line[4])
+		data13_list.append(split_line[5])
+		data14_list.append(split_line[6])
+# # #
+plt.grid(b=None, which='major', axis='both')
+plt.grid(color='grey')
+
+plt.plot(time_list, data4_list, c='red')
+plt.plot(time_list, data6_list, c='orange')
+plt.plot(time_list, data9_list, c='yellow')
+plt.plot(time_list, data12_list, c='green')
+plt.plot(time_list, data13_list, c='blue')
+plt.plot(time_list, data14_list, c='purple')
+
+plt.title('3.1.1 Unknown Substance ')
+plt.xlabel('Time (Seconds)')
+plt.ylabel('Tempurature (Celcius)')
+
+# # #
+plt.show()
+
+
+# # # # # # # # # #
