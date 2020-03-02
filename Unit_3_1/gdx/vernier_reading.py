5cef9aede3f7e1a6b5a4b31c75349c7ccab3ce32
Logan Hentschel <hentscht@student.psdr3.org>
2020-03-02 07:25:36 -0600

    Adding the additional help code into sensing collection/graphing

--- Unit_3_1/gdx/vernier_reading.py
+++ Unit_3_1/gdx/vernier_reading.py
@@ -1,15 +1,21 @@
-# Show a graph of the data
-# # # # #
+
 import gdx, time
 import matplotlib.pyplot as plt
-#
+# # # # #
 gdx = gdx.gdx()
+
 # # #
 gdx.open_usb()
+
+seconds = int(input("How many seconds do you want to take samples for?"))
+samples = int(input("How many samples do you want per second?"))
+period = 1000 / samples
+
+
 gdx.select_sensors([2,3])
-print('--- Please select time frame for data collection in miliseconds below --- \n 1000 miliseconds = 1 second')
-gdx.start()
-time = input('Please input the amount of samples wanted per second: ')
+gdx.start(period=period)
+#print('--- Please select time frame for data collection in miliseconds below --- \n 1000 miliseconds = 1 second')
+#time = input('Please input the amount of samples wanted per second: ')
 
 # # #
 for i in range(0,5):
@@ -22,7 +28,7 @@ for i in range(0,5):
 #for _ in range(time):
     #
 
-# # #
+# # # # #
 gdx.stop()
 gdx.close()
 
