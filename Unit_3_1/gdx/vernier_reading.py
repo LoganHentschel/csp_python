0a9ca75bd0cec5a36affefdcb2ffe644c9f4d9d6
Logan Hentschel <hentscht@student.psdr3.org>
2020-03-02 07:11:53 -0600

    directly collecting data from a sensor then creating a graph

--- Unit_3_1/gdx/vernier_reading.py
+++ Unit_3_1/gdx/vernier_reading.py
@@ -1,22 +1,28 @@
-# Ask user how long they want to collect data
-# Ask how many samples they want per second
-# -Collect for that long
-
 # Show a graph of the data
-
-import gdx #the gdx function calls are from a gdx.py file inside the gdx folder.
+# # # # #
+import gdx, time
+import matplotlib.pyplot as plt
+#
 gdx = gdx.gdx()
-
+# # #
 gdx.open_usb()
 gdx.select_sensors([2,3])
-gdx.start(period=1000) #1000 milliseconds = 1 second, period is amount of time between readings
+print('--- Please select time frame for data collection in miliseconds below --- \n 1000 miliseconds = 1 second')
+gdx.start()
+time = input('Please input the amount of samples wanted per second: ')
 
+# # #
 for i in range(0,5):
     measurements = gdx.read() #returns a list of measurements from the sensors selected.
     if measurements == None: 
         break 
     print(measurements)
 
+# # #
+#for _ in range(time):
+    #
+
+# # #
 gdx.stop()
 gdx.close()
 
