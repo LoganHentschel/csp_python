b4fa2753dd954a6d85ede7e93e9bf96ceb7e878b
Logan Hentschel <hentscht@student.psdr3.org>
2020-03-04 11:42:26 -0600

    finished graphing program: can collect data from sensors via python and display a graph of that data. Cannot loop graph yet

--- Unit_3_1/gdx/vernier_reading.py
+++ Unit_3_1/gdx/vernier_reading.py
@@ -1,25 +1,21 @@
 
 import gdx, time
 import matplotlib.pyplot as plt
-# # # # #
+
 gdx = gdx.gdx()
-
 # # #
-gdx.open_usb()
-
 seconds = int(input("How many seconds do you want to take samples for?"))
 samples = int(input("How many samples do you want per second?"))
 period = 1000 / samples
 
-
+gdx.open_usb()
 gdx.select_sensors([2,3])
 gdx.start(period=period)
-#print('--- Please select time frame for data collection in miliseconds below --- \n 1000 miliseconds = 1 second')
-#time = input('Please input the amount of samples wanted per second: ')
+
 
 # # #
 for i in range(0,5):
-    measurements = gdx.read() #returns a list of measurements from the sensors selected.
+    measurements = gdx.read()
     if measurements == None: 
         break 
     print(measurements)
