220ea404254ae9184cc64eba1dd82d9adb0d7131
Logan Hentschel <hentscht@student.psdr3.org>
2020-03-09 13:37:32 -0500

    instructions for own personal project idea. may do on side

--- CSP_Sem2/Unit_3_1/gdx/gui_interaction_project.py
+++ CSP_Sem2/Unit_3_1/gdx/gui_interaction_project.py
@@ -1,3 +1,7 @@
-#speaking moves a kirby sprite
-#either yelling or potentially using the grip sensor could make the sprite jump
+#speaking moves a kirby sprite to the right
+#using the grip sensor makes the sprite jump
+#an enemy sprite is in the middle of the screen. touching it means game over.
+#   - the player must jump over the enemy and get to the right side of the screen to an exit to win
+#   -  A start button sits in the top left corner, pressing it begins the game
+#   -  There would likely be constant gravity dragging the player down to the ground, but not going through the ground
 
