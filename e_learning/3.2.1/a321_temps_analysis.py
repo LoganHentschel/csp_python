cadeb92bad25029d1c7137108015e4115aa1190e
Logan Hentschel <hentscht@student.psdr3.org>
2020-04-02 00:00:42 -0500

    Got the graph to work; it shows up; data from the file's columns has been processed and created into the graph. no known errors yet, i think

--- e_learning/3.2.1/a321_temps_analysis.py
+++ e_learning/3.2.1/a321_temps_analysis.py
@@ -1,15 +1,17 @@
 # a321_temps_analysis.py
-# This program uses the pandas module to load a 2-dimensional data sheet into a pandas DataFrame object
+# This program uses the pandas module to load a 2-dimensional data-sheet into a pandas DataFrame object
 # Then it will use the matplotlib module to plot a graph and a bar chart
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Load in the data with read_csv()
-# TODO #1: change the file name to your data file name
-temp_data = pd.read_csv("temperature_data.csv", header=0)   # identify the header row
+# Load in the data with read_csv(), identifying the header row
+temp_data = pd.read_csv("temperature_data.csv", header=0)   
 
 # TODO #2: Use matplotlib to make a line graph
-
+plt.plot(temp_data['Year'], temp_data['Anomaly'], color='gray')
+plt.ylabel('Temperature Anomalies in Celsius')
+plt.xlabel('Years')
+plt.title('Change in Temperatures')
 
 # TODO #3: Plot LOWESS in a line graph
 
@@ -17,4 +19,8 @@ temp_data = pd.read_csv("temperature_data.csv", header=0)   # identify the heade
 # TODO #4: Use matplotlib to make a bar chart
 
 
-# TODO #5: Calculate min, max, and avg anomaly and the corresponding min/max years
+# TODO #5: Calculate min, max, and avg anomaly 
+# and the corresponding min/max years
+
+# # #
+plt.show()
